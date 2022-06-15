fd834e5256315cfa9056bc84d2b1b197d51d9ebe
simonzhangs <2863389578@qq.com>
2022-06-16 00:29:49 +0800

    feat: 初始化tabBar

---
+++ pages/955/data/index.js
@@ -0,0 +1,32 @@
+const companyList = [
+  {
+    name:'Afterpay',
+    location:'上海'
+  },
+  {
+    name:'Amazon',
+    location:'北京/上海'
+  },
+  {
+    name:'AMD',
+    location:'上海'
+  },
+  {
+    name:'ARM',
+    location:'上海'
+  },
+  {
+    name:'Airbnb',
+    location:'北京'
+  },
+  {
+    name:'zhihu',
+    location:'北京'
+  },
+  {
+    name:'douban',
+    location:'北京'
+  }
+]
+
+export default companyList;
